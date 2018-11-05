d06cfe34149aa13a9588bd55b6051323c7fdc0a3
gymie <jim8470@gmail.com>
2018-11-05 12:24:43 +0700

    Completed Form Login

--- AbsensiApp/application/controllers/Login.php
+++ AbsensiApp/application/controllers/Login.php
@@ -3,8 +3,61 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login extends CI_Controller {
 
+	public function __construct(){
+		parent::__construct();
+		$this->load->model("login_model");
+		$this->load->helper("security");
+		$this->load->library("form_validation");
+	}
+
+	private function _validate(){
+		$rules = array(
+			array(
+				"field" => "userid",
+				"label" => "User ID",
+				"rules" => "required|min_length[5]|max_length[20]"
+			),
+			array(
+				"field" => "password",
+				"label" => "Password",
+				"rules" => "required|min_length[5]"
+			)
+		);
+		$this->form_validation->set_rules($rules);
+		$this->form_validation
+			->set_error_delimiters("<span class='help-block'>",
+				"</span>");
+			
+		return $this->form_validation->run();
+	}
+
 	public function index()
 	{
 		$this->load->view('login_view');
+	}
+
+	public function ceklogin(){
+		if($this->input->post()){
+			if($this->_validate()){
+				$userid = $this->input->post("userid");
+				$password = do_hash($this->input->post("password"),
+								"md5");
+				$user = $this->login_model
+							->ambilUser($userid,$password);
+				if($user->num_rows() > 0){
+					redirect("beranda");
+				}else{
+					$this->session->set_flashdata("error-login",
+						"Userid dan Password Salah");
+					redirect("login");
+				}
+			}else{
+				$this->load->view("login_view");
+			}
+		}else{
+			$this->session->set_flashdata("error-login",
+				"Anda belum memasukkan Userid dan Password");
+			redirect("login");
+		}
 	}
 }
--- AbsensiApp/application/views/login_view.php
+++ AbsensiApp/application/views/login_view.php
@@ -5,21 +5,34 @@ $this->load->view("components/head");
     <div class="row">
         <div class="col-md-4 col-md-offset-4"
             style="margin-top: 15%">
-            <form action="beranda">
+            <?php
+            if($this->session->flashdata()){
+                echo "<div class='alert alert-danger alert-dismissible'>
+                        <button type='button' class='close' 
+                            data-dismiss='alert'><span>&times;</span>
+                        </button>
+                        ".$this->session->flashdata("error-login")."
+                    </div>";
+            }
+            ?>
+            <form action="login/ceklogin" method="POST">
                 <div class="panel panel-primary">
                     <div class="panel-heading">
                         <div class="panel-title">Form Login</div>
                     </div>
                     <div class="panel-body">
-                        <div class="form-group">
+                        <?php $has_error = validation_errors()?"has-error":""; ?>
+                        <div class="form-group <?= $has_error; ?>">
                             <label for="userid">User ID</label>
-                            <input type="text" class="form-control" id="userid"
+                            <input type="text" class="form-control" id="userid" name="userid"
                                 placeholder="masukkan userid anda"> 
+                            <?= form_error("userid"); ?>
                         </div> 
-                        <div class="form-group">
+                        <div class="form-group <?= $has_error; ?>">
                             <label for="password">Password</label>
-                            <input type="password" class="form-control" id="password"
+                            <input type="password" class="form-control" id="password" name="password"
                                 placeholder="masukkan password anda">
+                            <?= form_error("password"); ?>
                         </div>
                     </div>
                     <div class="panel-footer">
